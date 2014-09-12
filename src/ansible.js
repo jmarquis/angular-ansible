e108996551201edf58bda7c6e9e123ee6715e84a
Jeremy Marquis <jeremy@jeremymarquis.com>
2014-09-12 16:39:57 -0400

    more prospective work

--- src/ansible.js
+++ src/ansible.js
@@ -2,31 +2,75 @@ angular.module("ansible", [])
 
 	.provider("Ansible", function () {
 
-		var io;
+		var socket,
+			initialized = false,
+			queue = [];
+
+		var emit = function (name, data, callback) {
+			if (socket && socket.connected) {
+				socket.emit(name, data, callback);
+			} else {
+				queue.push({ name: name, data: data, callback: callback });
+			}
+		};
+
+		var processQueue = function () {
+			if (socket && socket.connected) {
+				while (queue.length > 0) {
+					var message = queue.shift();
+					socket.emit(message.name, message.data, message.callback);
+				}
+			}
+		};
 
 		return {
 
-			init: function (_io_) {
-				io = _io_;
+			init: function (_socket_) {
+
+				if (!initialized) {
+					socket = _socket_;
+
+					socket.on("connect", processQueue);
+
+					socket.on("ansible:update", function (data) {
+						console.log(data);
+					});
+
+					initialized = true;
+				}
+
 			},
 
 			$get: function ($q) {
 
-				var Ansible = function (room, callback) {
+				if (initialized) {
 
-					this.room = room;
-					this.get();
-					return this;
+					var Ansible = function (room, callback) {
 
-				};
+						this.room = room;
+						this.data = null;
 
-				Ansible.prototype.get = function () {
-					// request the remote model data
-				};
+						// request to join room, which will also trigger a data update
+						console.log("joining room: " + this.room);
+						emit("ansible:joinroom", this.room);
 
-				Ansible.prototype.save = function () {
-					// send the local model data
-				};
+						return this;
+
+					};
+
+					Ansible.prototype.get = function () {
+						// request the remote model data
+					};
+
+					Ansible.prototype.save = function () {
+						// send the local model data
+					};
+
+					return Ansible;
+
+				}
+
+				else throw "Ansible has not been initialized.";
 
 			}
 
